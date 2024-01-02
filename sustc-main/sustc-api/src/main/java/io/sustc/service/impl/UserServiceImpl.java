f60f9cfbe2d9473caa0da8396c7e13cff4c2b991
aurora324 <3050517330@qq.com>
2024-01-02 13:48:21 +0800

    co

--- sustc-main/sustc-api/src/main/java/io/sustc/service/impl/UserServiceImpl.java
+++ sustc-main/sustc-api/src/main/java/io/sustc/service/impl/UserServiceImpl.java
@@ -98,17 +98,17 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public boolean deleteAccount(AuthInfo auth, long mid) {
-        if (existMid(mid)) return false;
+        if (!existMid(mid)) return false;
 
         // auth is invalid
-        if (validAuth(auth)) return false;
+        if (!validAuth(auth)) return false;
 
         String sqlOfIdentity = "select identity as identity from users where mid = ? ";
         String identityOfAuth = "";
         try (Connection conn = dataSource.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sqlOfIdentity)) {
             stmt.setLong(1, auth.getMid());
-            ResultSet resultSet = stmt.executeQuery(sqlOfIdentity);
+            ResultSet resultSet = stmt.executeQuery();
             if (resultSet.next()) {
                 identityOfAuth = resultSet.getString("identity");
             }
@@ -121,7 +121,7 @@ public class UserServiceImpl implements UserService {
         try (Connection conn = dataSource.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sqlOfIdentity)) {
             stmt.setLong(1, mid);
-            ResultSet resultSet = stmt.executeQuery(sqlOfIdentity);
+            ResultSet resultSet = stmt.executeQuery();
             if (resultSet.next()) {
                 identity = resultSet.getString("identity");
             }
@@ -134,17 +134,114 @@ public class UserServiceImpl implements UserService {
         if (identityOfAuth.equals("user") && auth.getMid() != mid) {
             return false;
         }
-        if (identityOfAuth.equals("superuser") && (!identity.equals("user") || mid != auth.getMid())) {
+        if (identityOfAuth.equals("superuser") && (identity.equals("superUser") && mid != auth.getMid())) {
             return false;
+        }
+
+        String sqlOfDeleteCoin = "delete from coin where user_mid = ? ";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sqlOfDeleteCoin)) {
+            stmt.setLong(1, mid);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        String sqlOfDeleteDanmu = "delete from danmu where user_mid = ? ";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sqlOfDeleteDanmu)) {
+            stmt.setLong(1, mid);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        String sqlOfDeleteDanmulikeby = "delete from danmulikeby where mid = ? ";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sqlOfDeleteDanmulikeby)) {
+            stmt.setLong(1, mid);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        String sqlOfDeleteFavorite = "delete from favorite where user_mid = ? ";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sqlOfDeleteFavorite)) {
+            stmt.setLong(1, mid);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        String sqlOfDeleteFollow = "delete from coin where follower_mid = ? or following_mid = ? ";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sqlOfDeleteFollow)) {
+            stmt.setLong(1, mid);
+            stmt.setLong(2, mid);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        String sqlOfDeleteThumbs_up = "delete from thumbs_up where user_mid = ? ";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sqlOfDeleteThumbs_up)) {
+            stmt.setLong(1, mid);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        String sqlOfDeleteUser = "delete from user where mid = ? ";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sqlOfDeleteUser)) {
+            stmt.setLong(1, mid);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        String sqlOfDeleteVideo = "delete from video where owner_mid = ? ";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sqlOfDeleteVideo)) {
+            stmt.setLong(1, mid);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        String sqlOfDeleteView = "delete from view where user_mid = ? ";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sqlOfDeleteView)) {
+            stmt.setLong(1, mid);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
         return true;
     }
 
     @Override
     public boolean follow(AuthInfo auth, long followeeMid) {
-
         // auth is invalid
-        if (validAuth(auth)) return false;
+        if (!validAuth(auth)) return false;
+        if (auth.getMid() == followeeMid) return false;
+
+        // followeeMid valid
+        String sqlOfNumberOfFolloweeMidMid = "select count(*) as count from users where mid = ? ";
+        int numberOfFolloweeMid = 0;
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sqlOfNumberOfFolloweeMidMid)) {
+            stmt.setLong(1, followeeMid);
+            ResultSet resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                numberOfFolloweeMid = resultSet.getInt("count");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+        if (numberOfFolloweeMid != 1) return false;
 
         String sqlOfNumberOfMid = "select count(*) as count from follow where follower_mid = ? and following_mid = ?";
         int numberOfBoth = 0;
@@ -156,15 +253,33 @@ public class UserServiceImpl implements UserService {
             if (resultSet.next()) {
                 numberOfBoth = resultSet.getInt("count");
             }
-            resultSet.close();
-            stmt.close();
-            stmt.executeUpdate();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException();
         }
-        if (numberOfBoth != 1) return false;
 
-        return true;
+        if (numberOfBoth == 1) {
+            String sqlOfDelete = "delete from follow where follower_mid = ? and following_mid = ?";
+            try (Connection conn = dataSource.getConnection();
+                 PreparedStatement stmt = conn.prepareStatement(sqlOfDelete)) {
+                stmt.setLong(1, auth.getMid());
+                stmt.setLong(2, followeeMid);
+                stmt.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException();
+            }
+            return false;
+        } else {
+            String sqlOfInsert = "insert into follow values (? , ?)";
+            try (Connection conn = dataSource.getConnection();
+                 PreparedStatement stmt = conn.prepareStatement(sqlOfInsert)) {
+                stmt.setLong(1, auth.getMid());
+                stmt.setLong(2, followeeMid);
+                stmt.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
     }
 
     @Override
@@ -345,17 +460,17 @@ public class UserServiceImpl implements UserService {
         try (Connection conn = dataSource.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sqlOfMid)) {
             stmt.setLong(1, mid);
-            ResultSet resultSet = stmt.executeQuery(sqlOfMid);
+            ResultSet resultSet = stmt.executeQuery();
             if (resultSet.next()) {
                 numberOfMid = resultSet.getInt("count");
             }
-            resultSet.close();
-            stmt.close();
-            stmt.executeUpdate();
+//            resultSet.close();
+//            stmt.close();
+//            stmt.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return numberOfMid != 1;
+        return numberOfMid == 1;
     }
 
     public boolean existQQ(String QQ) {
@@ -397,8 +512,8 @@ public class UserServiceImpl implements UserService {
     }
 
     public boolean validAuth(AuthInfo auth) {
-        if ((auth.getQq() == null || auth.getQq().isEmpty()) && (auth.getWechat() == null || auth.getWechat().isEmpty()) && (auth.getPassword() == null || auth.getPassword().isEmpty()))
-            return false;
+//        if ((auth.getQq() == null || auth.getQq().isEmpty()) && (auth.getWechat() == null || auth.getWechat().isEmpty()) && (auth.getPassword() == null || auth.getPassword().isEmpty()))
+//            return false;
         //valid of mid
         boolean validOfMid = false;
         boolean validOfQQ = false;
@@ -406,7 +521,7 @@ public class UserServiceImpl implements UserService {
         int numberOfMid = 0;
         int numberOfQQ = 0;
         int numberOfWechat = 0;
-        if (auth.getPassword() != null && !auth.getPassword().isEmpty()) {
+        if (auth.getPassword() != null) {
             String sqlOfValidOfMid = "select count(*) as count from users where mid= ? and password = ?";
             try (Connection conn = dataSource.getConnection();
                  PreparedStatement stmt = conn.prepareStatement(sqlOfValidOfMid)) {
@@ -416,50 +531,78 @@ public class UserServiceImpl implements UserService {
                 if (resultSet.next()) {
                     numberOfMid = resultSet.getInt("count");
                 }
-                stmt.executeUpdate();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
             validOfMid = numberOfMid == 1;
+            if (numberOfMid != 1) return false;
         }
-
-
         //QQ is valid
-        if (auth.getQq() != null && !auth.getQq().isEmpty()) {
-            String sqlOfQQ = "select count(*) as count from users where mid= ? and QQ = ? ";
+        if (auth.getQq() != null) {
+            String sqlOfQQ = "select count(*) as count from users where QQ = ? ";
             try (Connection conn = dataSource.getConnection();
                  PreparedStatement stmt = conn.prepareStatement(sqlOfQQ)) {
-                stmt.setLong(1, auth.getMid());
-                stmt.setString(2, auth.getQq());
+                stmt.setString(1, auth.getQq());
                 ResultSet resultSet = stmt.executeQuery();
                 if (resultSet.next()) {
                     numberOfQQ = resultSet.getInt("count");
                 }
-                stmt.executeUpdate();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
             validOfQQ = numberOfQQ == 1;
-        }
 
+            if (validOfQQ) {
+                String sqlOfMid = "select mid as mid from users where QQ = ?";
+                try (Connection conn = dataSource.getConnection();
+                     PreparedStatement stmt = conn.prepareStatement(sqlOfMid)) {
+                    stmt.setString(1, auth.getQq());
+                    ResultSet resultSet = stmt.executeQuery();
+                    if (resultSet.next()) {
+                        long result = resultSet.getLong("mid");
+                        if (auth.getMid() == 0) auth.setMid(result);
+                        else {
+                            if (auth.getMid() != result) return false;
+                        }
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
         //wechat is valid
-        if (auth.getWechat() != null && !auth.getWechat().isEmpty()) {
-            String sqlOfWechat = "select count(*) as count from users where mid= ? and Wechat = ? ";
+        if (auth.getWechat() != null) {
+            String sqlOfWechat = "select count(*) as count from users where Wechat = ? ";
             try (Connection conn = dataSource.getConnection();
                  PreparedStatement stmt = conn.prepareStatement(sqlOfWechat)) {
-                stmt.setLong(1, auth.getMid());
-                stmt.setString(2, auth.getWechat());
+                stmt.setString(1, auth.getWechat());
                 ResultSet resultSet = stmt.executeQuery();
                 if (resultSet.next()) {
                     numberOfWechat = resultSet.getInt("count");
                 }
-                stmt.executeUpdate();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
             validOfWechat = numberOfWechat == 1;
+
+            if (validOfWechat) {
+                String sqlOfMid = "select mid as mid from users where wechat = ?";
+                try (Connection conn = dataSource.getConnection();
+                     PreparedStatement stmt = conn.prepareStatement(sqlOfMid)) {
+                    stmt.setString(1, auth.getWechat());
+                    ResultSet resultSet = stmt.executeQuery();
+                    if (resultSet.next()) {
+                        long result = resultSet.getLong("mid");
+                        if (auth.getMid() == 0) auth.setMid(result);
+                        else {
+                            if (auth.getMid() != result) return false;
+                        }
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
         return validOfMid || validOfQQ || validOfWechat;
     }
-
 }
